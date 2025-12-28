fd7388a0ad6c0dfc1e47a4dc8ec7db20fae7256d
salazaroliveros-prog <salazaroliveros@gmail.com>
2025-12-28 13:25:29 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './app.jsx';
 import './index.css'; // Esta línea activa todos los colores y efectos visuales
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <app />
   </React.StrictMode>,
 );
+
